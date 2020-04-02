550f9b6615a5c97aedb46190a4651f11863243c9
Romeomian <mianromeo10@gmail.com>
2020-04-02 15:28:19 -0400

    la reste du Modulo est <0>

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -6,9 +6,26 @@
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
 
 .NOTES
-    Author: CollegeBoreal
+    Author: 300104541romeomian
     Derniere mise à jour: yyyy-mm-dd
 
 #>
 
-Write-Host "Bonjour Monde"
+[String]$personneNom = "Alice"
+[Int]$personneAge = 35
+
+Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
+
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
+$villes = "Toronto","Mississauga","Scarborough","Brampton"
+for ($i = 0; $i -le $villes.Count; $i = $i + 1) {
+    $villes[$i]
+}
+
+"{0}" -F ($villes.Count % 2)
